db54c0a3ba2954dac8fd37fe65b9116b9dd6db24
Victor Aroucca <66020183+Aroucca@users.noreply.github.com>
2023-09-12 21:33:13 -0300

    Add files via upload

---
+++ inserts petshop.sql
@@ -0,0 +1,17 @@
+INSERT INTO Pessoas (nome, endereco, email, telefone)
+VALUES  
+		('Lucas ','Rua Jose Guerra,56', 'galinha123@gmail.com', 95236-5568), 
+		('Victor ','RUA SANTO ANTONIO - 554', 'SunnySally@dominio.com.', 93654-2897),
+        ('Hendrick ','RUA SAO PEDRO - 458', 'SmileySam@dominio.com.', 91467-3964),
+        ('Esdras ','RUA SETE DE SETEMBRO - 428', 'PrettyPolly@dominio.com.', 94856-3741),
+        ('Rafael ','RUA SAO PAULO - 619', 'CutiePatootie@dominio.com.', 9451-3690);
+        
+INSERT INTO pet (id_cliente, nome_pet, raca, pelagem, idade_pet, sexo)
+VALUES
+		(1 ,'Max', 'Beagle', 'Pelo Liso', 2, 'M'),
+        (1 ,'Mel', 'Husky', 'Pelo Duro', 10, 'F'),
+        (2 ,'Pimpão', 'Husky', 'Pelo Duro', 10, 'F'),
+        (3 ,'locao', 'Husky', 'Pelo Duro', 10, 'F'),
+        (4 ,'joca', 'Husky', 'Pelo Duro', 10, 'F'),
+        (5 ,'Temo', 'Husky', 'Pelo Duro', 10, 'F')
+
